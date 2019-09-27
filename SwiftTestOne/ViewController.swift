2dfe67c024cc7b8b2f96ca74acb0d5cca98764e6
ZongAng123 <zongang15@yahoo.com>
2019-09-27 09:45:09 +0800

    Initial Commit

---
+++ SwiftTestOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftTestOne
+//
+//  Created by 纵昂 on 2019/9/27.
+//  Copyright © 2019 纵昂 https://github.com/ZongAng123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftTestOneTests/SwiftTestOneTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftTestOneTests.swift
+//  SwiftTestOneTests
+//
+//  Created by 纵昂 on 2019/9/27.
+//  Copyright © 2019 纵昂 https://github.com/ZongAng123. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftTestOne
+
+class SwiftTestOneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
